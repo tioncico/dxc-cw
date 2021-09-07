038fda15c4734b56fd0e5cb5ab7f20d4af102d5d
tioncico <1067197739@qq.com>
2021-09-08 01:07:43 +0800

    fix

--- App/Service/Game/UserService.php
+++ App/Service/Game/UserService.php
@@ -56,12 +56,12 @@ class UserService extends BaseService
     {
         //血量公式计算=用户等级*100+用户耐力*100
         $hp = $userBaseAttributeInfo->level * 100 + $userBaseAttributeInfo->endurance * 100;
-        //蓝量计算公式= 100+(用户等级*10+用户智力*10);
-        $mp = 100 + ($userBaseAttributeInfo->level * 10 + $userBaseAttributeInfo->intellect * 10);
+        //蓝量计算公式= 100+((用户等级-1)*10+用户智力*10);
+        $mp = 100 + (($userBaseAttributeInfo->level-1) * 10 + $userBaseAttributeInfo->intellect * 10);
         //攻击力计算=1+(用户等级*5+用户力量*10)
         $attack = 1 + ($userBaseAttributeInfo->level * 5 + $userBaseAttributeInfo->strength * 10);
-        //防御力计算=0+(用户等级*2+用户耐力*10)
-        $defense = 0 + ($userBaseAttributeInfo->level * 2 + $userBaseAttributeInfo->endurance * 10);
+        //防御力计算=0+((用户等级-1)*2+用户耐力*10)
+        $defense = 0 + (($userBaseAttributeInfo->level-1) * 2 + $userBaseAttributeInfo->endurance * 10);
         $userBaseAttributeInfo->hp = $hp;
         $userBaseAttributeInfo->mp = $mp;
         $userBaseAttributeInfo->attack = $attack;
