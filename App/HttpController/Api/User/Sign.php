572bebbd3001a9472f8c843db9c80216f439f212
tioncico <1067197739@qq.com>
2021-09-16 21:13:28 +0800

    fix

--- App/HttpController/Api/User/Sign.php
+++ App/HttpController/Api/User/Sign.php
@@ -42,9 +42,10 @@ class Sign extends UserBase
      * @ApiSuccessParam(name="code",description="状态码")
      * @ApiSuccessParam(name="result",description="api请求结果")
      * @ApiSuccessParam(name="msg",description="api提示信息")
-     * @ApiSuccess({"code":200,"result":{"signRewardList":[{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":10},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":20},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":30},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":40},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":50},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":60},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":70}],"signNum":1},"msg":"查询成功","requestId":null})
+     * @ApiSuccess({"code":200,"result":{"signRewardList":[{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":10},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":20},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":30},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":40},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":50},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":60},{"goodsInfo":{"goodsId":2,"name":"钻石","code":"money","baseCode":null,"type":2,"description":"钻石,高级游戏货币","gold":0,"isSale":0,"level":1,"rarityLevel":5,"extraData":null},"num":70}],"signNum":0,"lastSignTime":0},"msg":"查询成功","requestId":null})
      * @ApiFail({"code":400,"result":[],"msg":"新增失败"})
      * @ApiSuccessParam(name="result.signNum",description="签到天数")
+     * @ApiSuccessParam(name="result.lastSignTime",description="最后签到的时间,如果断签则为0")
      * @ApiSuccessParam(name="result.signRewardList.[].signNum",description="签到奖励的天数")
      * @ApiSuccessParam(name="result.signRewardList.[].num",description="奖励的物品数量")
      * @ApiSuccessParam(name="result.signReward.[].goodsInfo.goodsId",description="物品id")
@@ -65,21 +66,24 @@ class Sign extends UserBase
         //当前用户签到天数
         $signInfo = UserSignModel::create()->get($this->who()->userId);
         $signNum = $signInfo->signNum;
+        $lastSignTime = $signInfo->lastUpdateTime ?? 0;
         //如果时间小于昨天
         if ($signInfo->lastUpdateTime < strtotime(date('Y-m-d', time() - 86400))) {
             $signNum = 0;
+            $lastSignTime = 0;
         }
         //获取money信息
         $moneyInfo = GoodsModel::create()->getInfoByCode('money');
         $list = [];
-        foreach ($signList as $key=>$sign){
+        foreach ($signList as $key => $sign) {
             $list[$key]['goodsInfo'] = $moneyInfo;
             $list[$key]['num'] = $sign['money'];
         }
 
         $data = [
             'signRewardList' => $list,
-            'signNum'        => $signNum
+            'signNum'        => $signNum,
+            'lastSignTime'   => $lastSignTime
         ];
 
         $this->writeJson(Status::CODE_OK, $data, "查询成功");
@@ -102,24 +106,24 @@ class Sign extends UserBase
         $model = new UserSignModel();
         $signInfo = $model->getInfo($this->who->userId);
         //如果是今天签到的
-        if ($signInfo->lastUpdateTime >= strtotime(date('Y-m-d'))){
-            Assert::assert(false,'你已签到');
+        if ($signInfo->lastUpdateTime >= strtotime(date('Y-m-d'))) {
+            Assert::assert(false, '你已签到');
         }
         //如果时间小于昨天
         if ($signInfo->lastUpdateTime < strtotime(date('Y-m-d', time() - 86400))) {
             $signNum = 1;
         }
         //获取钻石奖励
-        $signRewardInfo = SignRewardModel::create()->get(['signNum'=>($signNum%7)+1]);
-        Assert::assert(!!$signRewardInfo,'签到奖励配置不存在');
+        $signRewardInfo = SignRewardModel::create()->get(['signNum' => ($signNum % 7) + 1]);
+        Assert::assert(!!$signRewardInfo, '签到奖励配置不存在');
 
-        BaseModel::transaction(function ()use($signInfo,$signRewardInfo,$signNum){
+        BaseModel::transaction(function () use ($signInfo, $signRewardInfo, $signNum) {
             $signInfo->update([
-                'signNum'=>$signNum,
-                'lastUpdateTime'=>time()
+                'signNum'        => $signNum,
+                'lastUpdateTime' => time()
             ]);
             //增加钻石奖励
-            BackpackService::getInstance()->addMoney($this->who->userId,$signRewardInfo->money);
+            BackpackService::getInstance()->addMoney($this->who->userId, $signRewardInfo->money);
         });
 
         $this->writeJson(Status::CODE_OK, null, "签到成功,获得钻石*{$signRewardInfo->money}");
